9ebad6660ffa148c3c604e89d8849133e9a60be1
samarinha00 <salapjdm224@gmail.com>
2025-08-11 15:44:38 -0300

    finalizado

---
+++ seila0.2/Assets/scripts/MenuPrincipalManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+    
+    public void Jogar()
+    {
+        SceneManager.LoadScene("nomeDoLevelDeJogo");
+    }
+
+    public void AbrirOpcoes()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+    
+    public void FecharOpcoes()
+    {
+        painelOpcoes.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("Sair do Jogo");
+        Application.Quit();
+    }
+}
